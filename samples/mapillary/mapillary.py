02cdd4d8df626c344b974c9a24ed6458da0aca69
Julius Erbach <45179797+juliuserbach@users.noreply.github.com>
2020-03-31 21:42:32 +0200

    auto download coco wieghts

--- samples/mapillary/mapillary.py
+++ samples/mapillary/mapillary.py
@@ -33,6 +33,8 @@ import mrcnn.model as modellib
 
 # Path to trained weights file
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
 
 
 class mapvistas(Config):
