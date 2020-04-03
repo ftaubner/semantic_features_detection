dc72bee953fd6dcc9b4ace016a19dde0806c5fec
juliuserbach <erbachj@ethz.ch>
2020-04-03 21:20:29 +0200

    using shell script

---
+++ leonhard.sh
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+
+#HOME_DIR = '/cluster/home/erbachj'
+#LOCAL_SCRATCH = "/scratch"  
+#SCRATCH = '/cluster/scratch/erbachj'
+# Install required packages
+pip install -r /cluster/home/erbachj/requirements.txt
+
+cd /cluster/home/erbachj/semantic_features/semantic_features_detection
+python setup.py install
+
+# Get dataset and unzip it
+wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%2199865&authkey=ALxjJOloi9Wjk80" -O /scratch/mapillary_vistas_sample.zip
+unzip -qq /scratch/mapillary_vistas_sample.zip -d /scratch
+
+# run mapillary.py script
+python3 samples/mapillary/mapillary.py evaluate --dataset=/scratch/mapillary_vistas/ --model=coco --logs=/cluster/scratch/erbachj/semantic_features/logs
