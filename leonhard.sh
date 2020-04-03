94d6fe3daab458a9b7104dc010b06edd38e59160
juliuserbach <erbachj@ethz.ch>
2020-04-03 22:13:33 +0200

    created local python package for site-packages

--- leonhard.sh
+++ leonhard.sh
@@ -3,10 +3,10 @@
 #LOCAL_SCRATCH = "/scratch"  
 #SCRATCH = '/cluster/scratch/erbachj'
 # Install required packages
-pip install -r /cluster/home/erbachj/requirements.txt
+pip install --erbachj -r /cluster/home/erbachj/semantic_features/semantic_features_detection/requirements.txt
 
 cd /cluster/home/erbachj/semantic_features/semantic_features_detection
-python setup.py install
+python setup.py install --prefix=$HOME/python
 
 # Get dataset and unzip it
 wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%2199865&authkey=ALxjJOloi9Wjk80" -O /scratch/mapillary_vistas_sample.zip
