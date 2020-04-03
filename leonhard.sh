3033cc057757f9fcc799a697d585f73d1a1b2010
juliuserbach <erbachj@ethz.ch>
2020-04-03 23:25:04 +0200

    path correction

--- leonhard.sh
+++ leonhard.sh
@@ -10,7 +10,7 @@ python setup.py install --prefix=$HOME/python
 
 # Get dataset and unzip it
 wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%2199865&authkey=ALxjJOloi9Wjk80" -O $TMPDIR/mapillary_vistas_sample.zip
-unzip -qq $TMPDIR/mapillary_vistas_sample.zip -d /scratch
+unzip -qq $TMPDIR/mapillary_vistas_sample.zip -d $TMPDIR
 
 # run mapillary.py script
 python3 samples/mapillary/mapillary_leo.py train --dataset=$TMPDIR/mapillary_vistas --model=coco --logs=/cluster/scratch/erbachj/semantic_features/logs
