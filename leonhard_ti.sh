17c1ce98f7bd60528e9a36fceafe6e328f40f6af
juliuserbach <erbachj@ethz.ch>
2020-04-07 18:27:16 +0200

    changed to sample dataset

--- leonhard_ti.sh
+++ leonhard_ti.sh
@@ -9,8 +9,9 @@ cd /cluster/home/erbachj/semantic_features/semantic_features_detection
 python setup.py install --prefix=$HOME/python
 
 # Get dataset and unzip it
-wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%21100024&authkey=ADLJzehr2ENRggw" -O $TMPDIR/mapillary_vistas.zip
-unzip -qq $TMPDIR/mapillary_vistas.zip -d $TMPDIR
+#wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%21100024&authkey=ADLJzehr2ENRggw" -O $TMPDIR/mapillary_vistas.zip
+wget --no-check-certificate "https://onedrive.live.com/download?cid=EA356294C6263A37&resid=EA356294C6263A37%2199865&authkey=ALxjJOloi9Wjk80" -O $TMPDIR/mapillary_vistas.zip
+unzip -q $TMPDIR/mapillary_vistas.zip -d $TMPDIR
 
 # run mapillary.py script
 python3 samples/mapillary/mapillary_leo_ti.py train --dataset=$TMPDIR/mapillary_vistas --model=coco --logs=/cluster/scratch/erbachj/semantic_features/logs
